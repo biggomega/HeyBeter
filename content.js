762d94336d03362a30a8bb3e36a01338277a06cb
Ben Botvinick <benbotvinick@gmail.com>
2017-11-17 22:46:11 -0500

    Add files via upload

---
+++ content.js
@@ -0,0 +1,69 @@
+var elements = document.getElementsByTagName('*');
+
+for (var i = 0; i < elements.length; i++) {
+  var element = elements[i];
+
+  for (var j = 0; j < element.childNodes.length; j++) {
+    var node = element.childNodes[j];
+
+    if (node.nodeType === 3) {
+      var text = node.nodeValue;
+      var replacedText = text.replace(/ B/gi, ' 🅱️');
+
+      if (replacedText !== text) {
+          element.replaceChild(document.createTextNode(replacedText), node);
+      }
+    }
+  }
+}
+
+for (var i = 0; i < elements.length; i++) {
+  var element = elements[i];
+
+  for (var j = 0; j < element.childNodes.length; j++) {
+    var node = element.childNodes[j];
+
+    if (node.nodeType === 3) {
+      var text = node.nodeValue;
+      var replacedText = text.replace(/"B/gi, '"🅱️');
+
+      if (replacedText !== text) {
+          element.replaceChild(document.createTextNode(replacedText), node);
+      }
+    }
+  }
+}
+
+for (var i = 0; i < elements.length; i++) {
+  var element = elements[i];
+
+  for (var j = 0; j < element.childNodes.length; j++) {
+    var node = element.childNodes[j];
+
+    if (node.nodeType === 3) {
+      var text = node.nodeValue;
+      var replacedText = text.replace(/'B/gi, '\'🅱️');
+
+      if (replacedText !== text) {
+          element.replaceChild(document.createTextNode(replacedText), node);
+      }
+    }
+  }
+}
+
+for (var i = 0; i < elements.length; i++) {
+  var element = elements[i];
+
+  for (var j = 0; j < element.childNodes.length; j++) {
+    var node = element.childNodes[j];
+
+    if (node.nodeType === 3) {
+      var text = node.nodeValue;
+      var replacedText = text.replace(/Peter /gi, '🅱️eter ');
+
+      if (replacedText !== text) {
+          element.replaceChild(document.createTextNode(replacedText), node);
+      }
+    }
+  }
+}

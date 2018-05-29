a580b569f6e22781317681d1e8e659ad2818daa7
Ben Botvinick <benbotvinick@gmail.com>
2018-05-29 17:57:16 -0400

    Update background.js

--- hey-beter/background.js
+++ hey-beter/background.js
@@ -9,21 +9,21 @@ chrome.browserAction.onClicked.addListener(function() {
     let beter = items["hey-beter"];
     if (beter == 1) {
       chrome.storage.local.set({'hey-beter': 2});
-      chrome.browserAction.setIcon({path: 'image/switch/match-font.png'});
+      chrome.browserAction.setIcon({path: 'assets/image/switch/match-font.png'});
       chrome.browserAction.setTitle({title: "Click to disable"});
       chrome.tabs.executeScript({
         file: 'content.js'
       });
     } else if (beter == 2) {
       chrome.storage.local.set({'hey-beter': 0});
-      chrome.browserAction.setIcon({path: 'image/switch/disabled.png'});
+      chrome.browserAction.setIcon({path: 'assets/image/switch/disabled.png'});
       chrome.browserAction.setTitle({title: "Click to enable"});
       chrome.tabs.executeScript({
         file: 'content.js'
       });
     } else {
       chrome.storage.local.set({'hey-beter': 1});
-      chrome.browserAction.setIcon({path: 'image/icon19.png'});
+      chrome.browserAction.setIcon({path: 'assets/image/icon19.png'});
       chrome.browserAction.setTitle({title: "Click to match font"});
       chrome.tabs.executeScript({
         file: 'content.js'
